3514c3b255d1c5848b629a995c6df292f6ee3657
Jethro Arciaga <jet.arciaga@gmail.com>
2024-04-09 17:18:14 +0800

    Added functionality to update table from button
    Removed comments.
    Assigned OrderDetails widget to variable to pass to product widget
    
    Update the add_orders function from Product widget to update the
    table and the total amount.

--- asian_glow.py
+++ asian_glow.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable, List
 from time import localtime, strftime
-import functools
 
+# This will serve as our database
+ORDERS = {}
 
-ORDERS = {"Asian Glow": {"name": "Asian Glow Set", "quantity": 1, "price": 288}}
 
 @dataclass
 class ProfileWidget:
@@ -29,7 +29,6 @@ class ProfileWidget:
         img_dir = Path(__file__).resolve().parent / "img"
         self.profile_img = ImageTk.PhotoImage(Image.open(img_dir / filename).resize((200, 200)))
 
-
     def generate_widget(self):
         self._load_image("Profile.jpg")
         self.profile = tk.Label(self.frame, image=self.profile_img, text="Jethro Arciaga", compound=tk.BOTTOM)
@@ -43,6 +42,7 @@ class ProfileWidget:
 @dataclass
 class ProductWidget:
     master: tk.Tk
+    order_details_widget: 'OrderDetailsWidget'
 
     def __post_init__(self):
         self.frame = tk.Frame(self.master, bd=1)
@@ -61,14 +61,11 @@ class ProductWidget:
         button_config = {
             "frame": self.frame,
             "img": "",
-            "command": lambda: print("x"),
             "product": "unnamed"
         }
 
         def update_config(img: str, product: str, command=(lambda: print("x"))):
             nonlocal button_config
-
-
             button_config.update({
                 "img": self._load_image(img),
                 "product": product,
@@ -81,18 +78,18 @@ class ProductWidget:
             else:
                 ORDERS[name]["quantity"] += 1
                 ORDERS[name]["price"] = ORDERS[name]["quantity"] * price
-            print(ORDERS)
-
-
+            # Update ProductTable
+            self.order_details_widget.create_table(ORDERS)
+            # Update Total Amount
+            self.order_details_widget.update_total_amount(ORDERS)
 
         update_config(
             "asian_glow_kojic_avocado.jpg",
             "P100\nAsian Glow Kojic Avocado",
-            lambda: add_orders("Asian Glow Kojic Avocado", 288)
+            lambda: add_orders("Kojic Avocado", 100)
         )
         kojic_avocado = ImageButton(**button_config)
-        print(kojic_avocado)
-        kojic_avocado.grid_layout(0,1)
+        kojic_avocado.grid_layout(0, 1)
 
         update_config(
             "asian_glow_serum.jpg",
@@ -100,20 +97,31 @@ class ProductWidget:
             lambda: add_orders("Asian Glow Serum", 360)
         )
         serum = ImageButton(**button_config)
-        serum.grid_layout(1,1)
+        serum.grid_layout(1, 1)
 
-        update_config("asian_glow_set.jpg", "P288\nAsian Glow Ultimate Set")
+        update_config(
+            "asian_glow_set.jpg",
+            "P288\nAsian Glow Ultimate Set",
+            lambda: add_orders("Ultimate Set", 288)
+        )
         serum = ImageButton(**button_config)
-        serum.grid_layout(2,1)
+        serum.grid_layout(2, 1)
 
-        update_config("asian_glow_sunscreen.jpg", "P160\nAsian Premium Sunscreen")
+        update_config(
+            "asian_glow_sunscreen.jpg",
+            "P160\nAsian Glow Premium Sunscreen",
+            lambda: add_orders("Premium Sunscreen", 160)
+        )
         sunscreen = ImageButton(**button_config)
-        sunscreen.grid_layout(0,2)
+        sunscreen.grid_layout(0, 2)
 
-        update_config("asian_glow_toner.jpg", "P120\nAsian Glow Toner")
+        update_config(
+            "asian_glow_toner.jpg",
+            "P120\nAsian Glow Toner",
+            lambda: add_orders("Asian Glow Toner", 120)
+        )
         sunscreen = ImageButton(**button_config)
-        sunscreen.grid_layout(1,2)
-
+        sunscreen.grid_layout(1, 2)
 
 
 @dataclass
@@ -139,110 +147,70 @@ class ImageButton:
 
 
 @dataclass
-class ProductTable:
-    frame: tk.Frame
-    products: List[dict]
-
-    def __post_init__(self):
-        self.table_frame = tk.Frame(self.frame, bg="white", bd=1)
-        self.canvas = tk.Canvas(self.table_frame, bg="white")
-        self.scrollbar = tk.Scrollbar(
-            self.table_frame,
-            orient="vertical",
-            command=self.canvas.yview,
-            bg="white"
-        )
-        self.scrollable_frame = tk.Frame(self.canvas, bg="white")
-        self.scrollable_frame.bind(
-            "<Configure>",
-            lambda e: self.canvas.configure(
-                scrollregion=self.canvas.bbox("all")
-            )
-        )
-
-        self.canvas.create_window((0, 0), anchor="nw", window=self.scrollable_frame)
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        self.table_frame.grid(column=0, row=1)
-        self.canvas.grid(row=1, column=0, columnspan=3)
-        self.scrollbar.grid(column=4, row=1, sticky="ns")
-        self.create_table()
-
-    def create_table(self):
-        # Create table headers
-        headers = ["Product", "Quantity", "Price"]
-        for i, header in enumerate(headers):
-            label = tk.Label(self.table_frame, text=header, relief=tk.RIDGE, width=17)
-            label.grid(row=0, column=i)
-
-        # Populate products
-        for i, product in enumerate(self.products, 1):
-            print("HERE", product)
-            label_product = tk.Label(self.scrollable_frame, text=self.products[product]["name"], width=17, bg="white")
-            label_product.grid(row=i, column=0)
-
-            label_quantity = tk.Label(self.scrollable_frame, text=str(self.products[product]["quantity"]), bg="white", width=17)
-            label_quantity.grid(row=i, column=1)
-
-            label_price = tk.Label(self.scrollable_frame, text=str(self.products[product]["price"]), bg="white", width=17)
-            label_price.grid(row=i, column=2)
-
-
-
-        # for i, product in enumerate(self.products, 1):
-        #     label_product = tk.Label(self.scrollable_frame, text=product["name"], width=17, bg="white")
-        #     label_product.grid(row=i, column=0)
-
-        #     label_quantity = tk.Label(self.scrollable_frame, text=str(product["quantity"]), bg="white", width=17)
-        #     label_quantity.grid(row=i, column=1)
-
-        #     label_price = tk.Label(self.scrollable_frame, text=str(product["price"]), bg="white", width=17)
-        #     label_price.grid(row=i, column=2)
-
-
-@dataclass
 class OrderDetailsWidget:
     master: tk.Tk
 
     def __post_init__(self):
         self.frame = tk.Frame(self.master, bd=1)
         self.frame.grid(row=0, column=3, sticky="ns")
+        self.total_amount_label = tk.Label(self.frame, text=f"Total: ")
+        self.total_amount_label.grid(row=2, column=0, sticky="nw")
+        self.create_table(ORDERS)
         self.generate_widget()
-
 
     def generate_widget(self):
         label = tk.Label(self.frame, text="Order Details")
-        label.grid(row=0, column=0, sticky="ew")
-        # test_data = [
-        #     {"name": "Asian Glow Set", "quantity": 1, "price": 288},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Serum", "quantity": 2, "price": 360},
-        #     {"name": "Asian Glow Sunscreen", "quantity": 1, "price": 160}
-        # ]
-        ProductTable(self.frame, ORDERS)
+        label.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 
-        total_amount = tk.Label(self.frame, text=f"Total: ")
-        total_amount.grid(row=2, column=0)
+    def update_total_amount(self, products):
+        total_price = sum(product['price'] for product in products.values())
+        self.total_amount_label.config(text=f"Total: {total_price}")
 
+    def create_table(self, products):
+        table_frame = tk.Frame(self.frame, bg="white", bd=1)
+        table_frame.grid(row=1, column=0)
+        canvas = tk.Canvas(table_frame, bg="white")
+        scrollbar = tk.Scrollbar(
+            table_frame,
+            orient="vertical",
+            command=canvas.yview,
+            bg="white"
+        )
+        scrollable_frame = tk.Frame(canvas, bg="white")
+        scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
 
+        canvas.create_window((0, 0), anchor="nw", window=scrollable_frame)
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        canvas.grid(row=1, column=0, columnspan=3)
+        scrollbar.grid(column=4, row=1, sticky="ns")
+
+        # Create table headers
+        headers = ["Product", "Quantity", "Price"]
+        for i, header in enumerate(headers):
+            label = tk.Label(table_frame, text=header, relief=tk.RIDGE, width=17)
+            label.grid(row=0, column=i)
+
+        # Populate products
+        for i, product in enumerate(products.values(), 1):
+            label_product = tk.Label(scrollable_frame, text=product["name"], width=17, bg="white")
+            label_product.grid(row=i, column=0)
+
+            label_quantity = tk.Label(scrollable_frame, text=str(product["quantity"]), bg="white", width=17)
+            label_quantity.grid(row=i, column=1)
+
+            label_price = tk.Label(scrollable_frame, text=str(product["price"]), bg="white", width=17)
+            label_price.grid(row=i, column=2)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     ProfileWidget(root)
-    ProductWidget(root)
-    OrderDetailsWidget(root)
-
+    order_details_widget = OrderDetailsWidget(root)
+    product_widget = ProductWidget(root, order_details_widget)
     root.mainloop()
-
-
