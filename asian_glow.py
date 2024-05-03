6dd82738c5a06b81059fd7ac98342a3c41949495
Jethro Arciaga <jet.arciaga@gmail.com>
2024-05-03 12:39:02 +0800

    Optimize code and fix folder structure

--- asian_glow.py
+++ asian_glow.py
@@ -9,15 +9,16 @@ from datetime import datetime
 
 from PIL import Image, ImageTk
 
-# Important directory
-assets = Path(__file__).resolve().parent / "assets"
+# Important Directory
+CWD = Path(__file__).resolve().parent
+ASSETS = CWD / "assets"
 
 # This will serve as our database
 ORDERS = {}
 
-# This is the item price reference
+# Reference
 ITEMS_UNIT_PRICE = json.loads(
-    (assets / "unit_price.json").read_text(encoding="utf-8")
+    (ASSETS / "data" / "unit_price.json").read_text(encoding="utf-8")
 )
 
 
@@ -32,20 +33,18 @@ class ProfileWidget:
         self.update_time()
 
     def update_time(self):
-        current_time = strftime('%A, %d %B %Y\n %I:%M:%S %p', localtime())
+        current_time = strftime("%A, %d %B %Y\n %I:%M:%S %p", localtime())
         self.time_label.config(text=current_time)
-        self.time_label.grid(column=0, row=2)
         self.frame.after(1000, self.update_time)
 
     def _load_image(self, filename):
-        img_dir = Path(__file__).resolve().parent / "img"
-        self.profile_img = ImageTk.PhotoImage(Image.open(img_dir / filename).resize((200, 200)))
+        img_dir = ASSETS / "img" / filename
+        img = Image.open(img_dir).resize((200, 200))
+        self.profile_img = ImageTk.PhotoImage(img)
 
     def generate_widget(self):
         self._load_image("Profile.jpg")
-        self.profile = tk.Label(self.frame, image=self.profile_img,
-                                text="Jethro Arciaga", compound=tk.BOTTOM)
-        self.profile.image = self.profile_img
+        self.profile = tk.Label(self.frame, image=self.profile_img, text="KuyJets", compound=tk.BOTTOM)
         self.profile.grid(row=0, column=0)
 
         self.time_label = tk.Label(self.frame)
@@ -55,7 +54,7 @@ class ProfileWidget:
 @dataclass
 class ProductWidget:
     master: tk.Tk
-    order_details_widget: "OrderDetailsWidget"
+    orders_details_widget: "OrderDetailsWidget"
 
     def __post_init__(self):
         self.frame = tk.Frame(self.master, bd=1)
@@ -64,8 +63,7 @@ class ProductWidget:
 
     @staticmethod
     def _load_image(filename):
-        images = Path(__file__).resolve().parent / "img"
-        return images / filename
+        return ASSETS / "img" / filename
 
     def generate_widget(self):
         self.title = tk.Label(self.frame, text="Available Products")
@@ -91,10 +89,10 @@ class ProductWidget:
             else:
                 ORDERS[name]["quantity"] += 1
                 ORDERS[name]["price"] = ORDERS[name]["quantity"] * price
-            # Update ProductTable
-            self.order_details_widget.create_table(ORDERS)
+            # Update Product Table
+            self.orders_details_widget.create_table(ORDERS)
             # Update Total Amount
-            self.order_details_widget.update_total_amount(ORDERS)
+            self.orders_details_widget.update_total_amount(ORDERS)
 
         update_config(
             "asian_glow_kojic_avocado.jpg",
@@ -117,8 +115,8 @@ class ProductWidget:
             "P288\nAsian Glow Ultimate Set",
             lambda: add_orders("Ultimate Set", 288)
         )
-        serum = ImageButton(**button_config)
-        serum.grid_layout(2, 1)
+        asian_glow_set = ImageButton(**button_config)
+        asian_glow_set.grid_layout(2, 1)
 
         update_config(
             "asian_glow_sunscreen.jpg",
@@ -148,7 +146,7 @@ class ImageButton:
         self.image = ImageTk.PhotoImage(Image.open(self.img).resize((200, 200)))
         self.button = tk.Button(
             self.frame,
-            image=self.image,
+            image = self.image,
             text=self.product,
             compound=tk.TOP,
             command=self.command
@@ -166,12 +164,14 @@ class OrderDetailsWidget:
     def __post_init__(self):
         self.frame = tk.Frame(self.master, bd=1)
         self.frame.grid(row=0, column=3, sticky="ns")
+        self.total_amount_label= tk.Label(self.frame, text="Total: ")
+        self.total_amount_label.grid(row=2, column=0, sticky="nw")
         self.create_table(ORDERS)
         self.generate_widget()
 
     def receipt_window(self):
         if len(ORDERS) < 1:
-            messagebox.showerror("Receipt Error", "Nothing to generate.")
+            messagebox.showerror("Receipt Error", "Nothing to generate")
             return
 
         receipt_window = tk.Toplevel(self.frame)
@@ -187,6 +187,13 @@ class OrderDetailsWidget:
     def modify_transaction_window(self):
         temp_order = ORDERS.copy()
 
+        modify_window = tk.Toplevel(self.frame)
+        modify_window.title("Edit transaction window")
+        modify_window.grab_set()
+
+        product_listbox = tk.Listbox(modify_window, selectmode=tk.SINGLE, width=40)
+        product_listbox.grid(row=0, column=0, padx=5, pady=5, columnspan=3)
+
         def refresh_window():
             selection = product_listbox.curselection()
             product_listbox.delete(0, "end")
@@ -195,23 +202,12 @@ class OrderDetailsWidget:
             if selection:
                 product_listbox.selection_set(selection[0])
 
-        def confirm_changes(orders):
-            global ORDERS
-            for order in orders.copy():
-                if orders[order]["quantity"] == 0:
-                    del orders[order]
-
-            ORDERS = orders
-            modify_window.destroy()
-            self.create_table(ORDERS)
-            self.update_total_amount(ORDERS)
-
         def add_item(orders):
             selection = product_listbox.curselection()
             if len(selection) == 0:
                 messagebox.showerror("Selection Error.", "Please select an item to modify.")
 
-            if selection:
+            elif selection:
                 index = selection[0]
                 selected_item = product_listbox.get(index)
                 key = selected_item.split("|")[0].strip()
@@ -225,25 +221,28 @@ class OrderDetailsWidget:
             if len(selection) == 0:
                 messagebox.showerror("Selection Error.", "Please select an item to modify.")
 
-            if selection:
+            elif selection:
                 index = selection[0]
                 selected_item = product_listbox.get(index)
                 key = selected_item.split("|")[0].strip()
                 if orders[key]["quantity"] == 0:
-                    messagebox.showerror("Error", "If quantity is 0 the product will be \
-                                         deleted from the transaction.")
+                    messagebox.showerror("Error", "If quantity is 0 the product will be deleted.")
                 else:
                     unit_price = ITEMS_UNIT_PRICE[key]
                     orders[key]["quantity"] -= 1
                     orders[key]["price"] = unit_price * orders[key]["quantity"]
             refresh_window()
 
-        modify_window = tk.Toplevel(self.frame)
-        modify_window.title("Edit transaction window")
-        modify_window.grab_set()
+        def confirm_changes(orders):
+            global ORDERS
+            for order in orders.copy():
+                if orders[order]["quantity"] == 0:
+                    del orders[order]
 
-        product_listbox = tk.Listbox(modify_window, selectmode=tk.SINGLE, width=40)
-        product_listbox.grid(row=0, column=0, padx=5, pady=5, columnspan=3)
+            ORDERS = orders
+            modify_window.destroy()
+            self.create_table(ORDERS)
+            self.update_total_amount(ORDERS)
 
         subtract_button = tk.Button(
             modify_window,
@@ -252,8 +251,8 @@ class OrderDetailsWidget:
             height=1,
             font=("Helvetica", 20),
             command=lambda: subtract_item(temp_order)
-            )
-        subtract_button.grid(row=1, column=0, pady=3)
+        )
+        subtract_button.grid(row=1, column=0, padx=3)
 
         add_button = tk.Button(
             modify_window,
@@ -262,8 +261,8 @@ class OrderDetailsWidget:
             height=1,
             font=("Helvetica", 20),
             command=lambda: add_item(temp_order)
-            )
-        add_button.grid(row=1, column=1, pady=3)
+        )
+        add_button.grid(row=1, column=1, padx=3)
 
         confirm_button = tk.Button(
             modify_window,
@@ -273,16 +272,13 @@ class OrderDetailsWidget:
             font=("Helvetica", 20),
             fg="green",
             command=lambda: confirm_changes(temp_order)
-            )
-        confirm_button.grid(row=1, column=2, pady=3)
+        )
+        confirm_button.grid(row=1, column=2, padx=3)
         refresh_window()
 
     def generate_widget(self):
-        label = tk.Label(self.frame, text="Transaction Details")
-        label.grid(row=0, column=0, columnspan=3, sticky="ew", padx=5, pady=5)
-
-        self.total_amount_label = tk.Label(self.frame, text="Total: ")
-        self.total_amount_label.grid(row=2, column=0, sticky="nw")
+        title = tk.Label(self.frame, text="Transaction Details")
+        title.grid(row=0, column=0, columnspan=3, sticky="ew", padx=5, pady=5)
 
         edit_transaction = tk.Button(self.frame, text="Edit transaction",
                                      command=self.modify_transaction_window,
@@ -293,57 +289,47 @@ class OrderDetailsWidget:
         receipt_button.grid(row=2, column=2, padx=1, pady=4)
 
     def update_total_amount(self, products):
-        total_price = sum(product['price'] for product in products.values())
+        total_price = sum(product["price"] for product in products.values())
         self.total_amount_label.config(text=f"Total: {total_price}")
         return total_price
 
-    def create_table(self, products, receipt=False, headers=True, frame=None):
+    def create_table(self, products, receipt=False, frame=None, headers=True):
         start = 1
         if frame is None:
             frame = self.frame
         if not headers:
             start = 2
-        table_frame = tk.Frame(frame, bg="white", bd=1)
+
+        table_frame = tk.Frame(frame, bd=1)
         table_frame.grid(row=1, column=0, columnspan=3)
         canvas = tk.Canvas(table_frame, bg="white")
-        scrollbar = tk.Scrollbar(
-            table_frame,
-            orient="vertical",
-            command=canvas.yview,
-            bg="white"
-        )
-        scrollable_frame = tk.Frame(canvas, bg="white")
-        scrollable_frame.bind(
-            "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
-            )
-        )
+        scrollbar = tk.Scrollbar(table_frame, orient="vertical",
+                                 command=canvas.yview, bg="white")
 
+        scrollable_frame = tk.Frame(canvas, bg="white")
+        scrollable_frame.bind("<Configure>",
+                              lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
         canvas.create_window((0, 0), anchor="nw", window=scrollable_frame)
         canvas.configure(yscrollcommand=scrollbar.set)
-
         canvas.grid(row=1, column=0, columnspan=3)
-        scrollbar.grid(column=4, row=1, sticky="ns")
+        scrollbar.grid(row=1, column=4, sticky="ns")
 
         # Create table headers
         if headers:
             headers = ["Product", "Quantity", "Price"]
             for i, header in enumerate(headers):
-                label = tk.Label(table_frame, text=header, relief=tk.RIDGE, width=17)
+                label = tk.Label(table_frame, text=header, relief=tk.RAISED, width=17)
                 label.grid(row=0, column=i)
 
         # Populate products
         for i, product in enumerate(products.values(), start=start):
-            label_product = tk.Label(scrollable_frame, text=product["name"], width=17, bg="white")
+            label_product = tk.Label(scrollable_frame, text=product["name"], bg="white", width=17)
             label_product.grid(row=i, column=0)
 
-            label_quantity = tk.Label(scrollable_frame, text=str(product["quantity"]),
-                                      bg="white", width=17)
+            label_quantity = tk.Label(scrollable_frame, text=product["quantity"], bg="white", width=17)
             label_quantity.grid(row=i, column=1)
 
-            label_price = tk.Label(scrollable_frame, text=str(product["price"]),
-                                   bg="white", width=17)
+            label_price = tk.Label(scrollable_frame, text=product["price"], bg="white", width=17)
             label_price.grid(row=i, column=2)
 
         if receipt:
@@ -352,10 +338,11 @@ class OrderDetailsWidget:
             total_amount.grid(row=i+1, column=2)
 
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Asian Glow")
     ProfileWidget(root)
-    order_details_widget = OrderDetailsWidget(root)
-    ProductWidget(root, order_details_widget)
+    orders_details_widget = OrderDetailsWidget(root)
+    ProductWidget(root, orders_details_widget)
     root.mainloop()
